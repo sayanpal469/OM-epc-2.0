462fbf2d05a1a541a7431fc1a9f806719e39b187
sayanpal469 <sayanpal469@gmail.com>
2024-01-01 14:35:13 +0530

    whatsapp qr installed

--- src/pages/Admin/Qr_code.jsx
+++ src/pages/Admin/Qr_code.jsx
@@ -1,50 +1,63 @@
-import { useState } from "react"
-import QRCode from "react-qr-code"
-import { GET_WHATSAPP_CODE } from "../../graphql/queries/graphql_queries"
-import { useQuery } from "@apollo/client"
+import { useEffect, useState } from "react";
+import QRCode from "react-qr-code";
+import { GET_WHATSAPP_CODE } from "../../graphql/queries/graphql_queries";
+import { useLazyQuery } from "@apollo/client";
 
 const Qr_code = () => {
-    const [showQrCode, setShowQrCode] = useState(false)
-    const [value, setValue] = useState("")
+  // const [showQrCode, setShowQrCode] = useState(false);
+  const [value, setValue] = useState("");
 
-    const { data, error } = useQuery(GET_WHATSAPP_CODE, {
-        context: {
-          headers: {
-            authorization: `${localStorage.getItem("token")}`,
-          },
-        },
-      });
+  const [getWhatsappCode, { data, loading }] = useLazyQuery(GET_WHATSAPP_CODE, {
+    context: {
+      headers: {
+        authorization: `${localStorage.getItem("token")}`,
+      },
+    },
+  });
 
-      console.log({data})
-      console.log(error)
-      
-    const handleButtonClick = ()=>{
-        setShowQrCode(true)
-        setValue(data)
+  const handleButtonClick = () => {
+    getWhatsappCode();
+    // setShowQrCode(true);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setValue(data?.getQRCode);
+      console.log(data);
     }
+  }, [data]);
+
   return (
     <div>
-       <div className="flex justify-center items-center h-screen">
-      {showQrCode ? (
-        <div className="ml-8 my-2">
-          <QRCode
-            size={250}
-            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-            value={value}
-            viewBox={`0 0 256 256`}
-          />
-        </div>
-      ) : (
+      <div className="flex justify-center items-center h-screen">
         <button
           className="sm:w-auto mt-4 sm:mt-0 px-4 py-2 rounded-md bg-blue-500 text-white"
           onClick={handleButtonClick}
         >
           Get QR code
         </button>
-      )}
+        <div className="ml-8 my-2">
+          {loading ? (
+            <div>
+              <div
+                className="w-12 h-12 rounded-full animate-spin
+                    border-solid border-8 border-purple-500 border-t-transparent"
+              ></div>
+            </div>
+          ) : (
+            value && (
+              <QRCode
+                size={250}
+                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                value={value}
+                viewBox={`0 0 256 256`}
+              />
+            )
+          )}
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Qr_code
+export default Qr_code;
