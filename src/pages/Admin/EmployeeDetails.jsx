c00e2122585890bf7f9a6f638b99b670a945d635
unknown <dsubhojit063@gmail.com>
2024-03-08 23:21:22 +0530

    graphd asdded

--- src/pages/Admin/EmployeeDetails.jsx
+++ src/pages/Admin/EmployeeDetails.jsx
@@ -1,13 +1,17 @@
-import { useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 import PropTypes from "prop-types";
-import { GET_CALLS_BY_ENGINEER } from "../../graphql/queries/graphql_queries";
+import {
+  GET_ATTENDENCE_BY_ENG,
+  GET_CALLS_BY_ENGINEER,
+  GET_EXPENSE_BY_ENG,
+} from "../../graphql/queries/graphql_queries";
 import { Fragment, useEffect, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import EngineerGraph from "./EngineerGraph";
 
 const EmployeeDetails = ({ open, setOpen, EMP_id }) => {
   const [calls, setCalls] = useState([]);
- 
-  
+
   const cancelButtonRef = useRef(null);
   const { data } = useQuery(GET_CALLS_BY_ENGINEER, {
     variables: {
@@ -20,64 +24,92 @@ const EmployeeDetails = ({ open, setOpen, EMP_id }) => {
       },
     },
   });
- 
+
+  const [getAttendenceByEng, { data: attendenceData }] = useLazyQuery(
+    GET_ATTENDENCE_BY_ENG,
+    {
+      context: {
+        headers: {
+          authorization: `${localStorage.getItem("token")}`,
+        },
+      },
+    }
+  );
+
+  const [getExpenseByEng, { data: expenseData }] = useLazyQuery(
+    GET_EXPENSE_BY_ENG,
+    {
+      context: {
+        headers: {
+          authorization: `${localStorage.getItem("token")}`,
+        },
+      },
+    }
+  );
 
   useEffect(() => {
     if (data) {
       setCalls(data?.callsByEng?.call_list);
     }
+    getAttendenceByEng({
+      variables: {
+        engEmp: EMP_id,
+      },
+    });
+    getExpenseByEng({
+      variables: {
+        engEmp: EMP_id,
+      },
+    });
   }, [data]);
-
-  const pendingCalls = calls.filter((call) => call.status == "PENDING");
-  const completedCalls = calls.filter((call) => call.status === "COMPLETED");
-
-  // console.log(data?.callsByEng?.call_list);
 
   return (
     <>
-   <div>
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        initialFocus={cancelButtonRef}
-        onClose={setOpen}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div>
+        <Transition.Root show={open} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-10"
+            initialFocus={cancelButtonRef}
+            onClose={setOpen}
+          >
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
               leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
             >
-             
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl my-10">
-          
-<div className="py-2">
-  <div className="text-center text-4xl sm:text-5xl mt-4">
-    <h1 className="font-bold">Souvik Das</h1>
-  </div>
-  <div className="flex flex-col sm:flex-row justify-between mx-4 sm:mx-16 my-8 px-4 sm:px-8">
-    <div className="text-lg sm:text-xl font-bold border-red-200 border-2 w-72 sm:w-96 mb-4 sm:mb-0">
-      graphhh
-    </div>
-    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 text-lg sm:text-xl my-2">
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl my-10">
+                    <div className="py-2">
+                      <div className="text-center text-4xl sm:text-5xl mt-4">
+                        <h1 className="font-bold">Souvik Das</h1>
+                      </div>
+                      <div className="flex flex-col sm:flex-row justify-between mx-4  px-4 ">
+                        <div className="text-lg sm:text-xl font-bold  mb-4 sm:mb-0">
+                          <EngineerGraph
+                            calls={calls}
+                            attendenceData={attendenceData}
+                            expenseData={expenseData}
+                          />
+                        </div>
+                        {/* <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 text-lg sm:text-xl my-2">
       {data ? (
         <div className="mt-3 sm:ml-4 sm:mt-0 my-4">
           <div>
@@ -110,26 +142,25 @@ const EmployeeDetails = ({ open, setOpen, EMP_id }) => {
           </p>
         </div>
       </div>
-    </div>
-  </div>
-  <div className="px-4 py-2 sm:flex sm:flex-row-reverse justify-center">
-    <button
-      type="button"
-      className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm sm:text-base font-semibold text-white shadow-sm hover:bg-red-500 sm:w-auto"
-      onClick={() => setOpen(false)}
-    >
-      Cancel
-    </button>
-  </div>
-</div>
-
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
-    </div>
+    </div> */}
+                      </div>
+                      <div className="px-4 py-2 sm:flex sm:flex-row-reverse justify-center">
+                        <button
+                          type="button"
+                          className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm sm:text-base font-semibold text-white shadow-sm hover:bg-red-500 sm:w-auto"
+                          onClick={() => setOpen(false)}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+      </div>
     </>
   );
 };
---
+++ src/pages/Admin/EngineerGraph.jsx
@@ -0,0 +1,207 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  BarChart,
+  Bar,
+  ResponsiveContainer,
+} from "recharts";
+import { isArray } from "@apollo/client/utilities";
+
+const EngineerGraph = ({ calls, attendenceData, expenseData }) => {
+  const [attendence, setAttendence] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [expense, setExpense] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const test = (data) => {
+    const currentYear = new Date().getFullYear();
+    const monthCounts = Array.from({ length: 12 }, () => 0);
+
+    data.forEach((call) => {
+      const submitDate = call.submit_date;
+      const submitYear = parseInt(submitDate.split("-")[2]);
+      const submitMonth = parseInt(submitDate.split("-")[1], 10) - 1; // Adjust for zero-based index
+      if (submitYear === currentYear) {
+        monthCounts[submitMonth]++;
+      }
+    });
+
+    return monthCounts;
+  };
+
+  const calculateTotalAttendanceByMonth = (attendanceArr) => {
+    const currentYear = new Date().getFullYear();
+    const monthlyAttendance = Array(12).fill(0);
+
+    attendanceArr.forEach((entry) => {
+      const dateParts = entry.date.split("/");
+      const submitYear = parseInt(dateParts[2]);
+      const month = parseInt(dateParts[1], 10) - 1; // Month is zero-based in JavaScript Date object
+
+      // Check if the date has already been counted for this month
+      const alreadyCounted =
+        monthlyAttendance[month] &&
+        monthlyAttendance[month].includes(entry.date);
+      if (submitYear === currentYear) {
+        if (!alreadyCounted) {
+          if (!monthlyAttendance[month]) {
+            monthlyAttendance[month] = [entry.date];
+          } else {
+            monthlyAttendance[month].push(entry.date);
+          }
+        }
+      }
+    });
+
+    // Map the array to get the count for each month
+    const counts = monthlyAttendance.map((monthArray) =>
+      monthArray ? monthArray.length : 0
+    );
+
+    return counts;
+  };
+
+  const calciExpense = (expenseArr) => {
+    const currentYear = new Date().getFullYear();
+    return expenseArr.reduce((result, expense) => {
+      if (expense.status === "APPROVE") {
+        const monthIndex = parseInt(expense.date.split("-")[1], 10) - 1;
+        const year = parseInt(expense.date.split("-")[2]);
+
+        // Check if the month is valid (1 to 12)
+        if (
+          !isNaN(monthIndex) &&
+          monthIndex >= 0 &&
+          monthIndex < 12 &&
+          year === currentYear
+        ) {
+          result[monthIndex] += parseInt(expense.expense_amount, 10);
+        }
+      }
+      return result;
+    }, Array(12).fill(0));
+  };
+
+  const monthCounts = test(calls);
+  //   const attendenceCounts = test2(attendenceData);
+  useEffect(() => {
+    if (attendenceData?.getAttendenceByEng?.attendence?.length > 0) {
+      //   console.log({ attendenceData });
+      const attendenceCounts = calculateTotalAttendanceByMonth(
+        attendenceData.getAttendenceByEng.attendence
+      );
+      setAttendence(attendenceCounts);
+      //   console.log({ attendenceCounts });
+    }
+  }, [attendenceData]);
+
+  useEffect(() => {
+    if (expenseData) {
+      const result = calciExpense(expenseData.expenseReportByEng?.expense_list);
+      console.log({ result });
+      setExpense(result);
+    }
+  }, [expenseData]);
+
+  const data = [
+    {
+      name: "Jan",
+      call: monthCounts[0],
+      expense: expense[0],
+      attendence: attendence[0],
+    },
+    {
+      name: "FEB",
+      call: monthCounts[1],
+      expense: expense[1],
+      attendence: attendence[1],
+    },
+    {
+      name: "Mar",
+      call: monthCounts[2],
+      expense: expense[2],
+      attendence: attendence[2],
+    },
+    {
+      name: "Apr",
+      call: monthCounts[3],
+      expense: expense[3],
+      attendence: attendence[3],
+    },
+    {
+      name: "May",
+      call: monthCounts[4],
+      expense: expense[4],
+      attendence: attendence[4],
+    },
+    {
+      name: "Jun",
+      call: monthCounts[5],
+      expense: expense[5],
+      attendence: attendence[5],
+    },
+    {
+      name: "Jul",
+      call: monthCounts[6],
+      expense: expense[6],
+      attendence: attendence[6],
+    },
+    {
+      name: "Aug",
+      call: monthCounts[7],
+      expense: expense[7],
+      attendence: attendence[7],
+    },
+    {
+      name: "Sept",
+      call: monthCounts[8],
+      expense: expense[8],
+      attendence: attendence[8],
+    },
+    {
+      name: "Oct",
+      call: monthCounts[9],
+      expense: expense[9],
+      attendence: attendence[9],
+    },
+    {
+      name: "Nov",
+      call: monthCounts[10],
+      expense: expense[10],
+      attendence: attendence[10],
+    },
+    {
+      name: "Dec",
+      call: monthCounts[11],
+      expense: expense[11],
+      attendence: attendence[11],
+    },
+  ];
+
+  return (
+    <div>
+      <BarChart width={820} height={450} data={data}>
+        <CartesianGrid strokeDasharray="10 10" />
+        <XAxis dataKey="name" />
+        {/* <YAxis type="number" domain={[100, 500]} /> */}
+        <Tooltip />
+        {/* <Legend /> */}
+        <Bar dataKey="expense" fill="#8884d8" />
+        <Bar dataKey="call" fill="#82ca9d" />
+        <Bar dataKey="attendence" fill="#DC143C" />
+      </BarChart>
+    </div>
+  );
+};
+
+EngineerGraph.propTypes = {
+  calls: PropTypes.any,
+  attendenceData: PropTypes.any,
+  expenseData: PropTypes.any,
+};
+
+export default EngineerGraph;
